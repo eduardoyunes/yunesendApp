49783d04ca3e41cef390e6880f1d012ebd78ee81
Eduardo Bardou Yunes Filho <37004473+eduardoyunes@users.noreply.github.com>
2018-10-16 01:18:51 -0300

    First Commit

---
+++ tools/build/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    module: {
+      rules: [
+        /**
+         * tslint-loader
+         *
+         * See: https://github.com/wbuchwalter/tslint-loader
+         */
+        {
+          enforce: 'pre',
+          test: /\.ts$/,
+          use: 'tslint-loader',
+          exclude: [
+            'node_modules',
+            /\.(ngfactory|ngstyle)\.ts$/
+          ]
+        },
+      ]
+    }
+  };
+  
